7a3102adfef2ff236f6ce3bb10334db3109ef145
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-16 09:18:47 +0800

    refactor: debug group is now its own component

---
+++ src/components/DebugGroup.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { LayoutChangeEvent } from "react-native";
+import { Canvas, Circle, Group, Rect, Selector, SkSize, SkiaValue, Text, interpolate, mix, point, size, useClockValue, useComputedValue, useFont, useSharedValueEffect, useValue, useValueEffect } from "@shopify/react-native-skia";
+import { Vector2, Vector2Zero } from "src/core";
+import { RoundedBox } from "./RoundedBox";
+
+interface DebugGroupProps {
+    canvasSize: SkiaValue<SkSize>
+}
+
+export const DebugGroup = ({ canvasSize }: DebugGroupProps) => {
+    const fontSize = 24;
+    const font = useFont(require("assets/fonts/Roboto/Roboto-Regular.ttf"), fontSize);
+
+    const clock = useClockValue();
+    const previousClock = useValue(0);
+
+    const FPS = useValue(0);
+    const FPS_Display = useComputedValue(() => `FPS: ${FPS.current}`, [FPS])
+
+    // Called every frame
+    useValueEffect(clock, () => {
+        const deltaTime = clock.current - previousClock.current;
+        const calculatedFPS = Math.round(1000 / (deltaTime));
+
+        // console.log(calculatedFPS);
+
+        FPS.current = calculatedFPS;
+        previousClock.current = clock.current;
+    });
+
+    const canvasCenter = useComputedValue(() => {
+        return point(canvasSize.current.width / 2, canvasSize.current.height / 2)
+    }, [canvasSize]);
+
+    const dotSize = size(10, 10);
+
+    if (font == null) return null;
+    return (
+        <Group color={"red"}>
+            <Text x={10} y={30} text={FPS_Display} font={font} />
+            {/* Top Dot */}
+            <Rect
+                x={Selector(canvasCenter, v => v.x - dotSize.width / 2)}
+                y={-dotSize.height / 2}
+                width={dotSize.width}
+                height={dotSize.height}
+            />
+
+            {/* Middle Dot */}
+            <Rect
+                x={Selector(canvasCenter, v => v.x - dotSize.width / 2)}
+                y={Selector(canvasCenter, v => v.y - dotSize.height / 2)}
+                width={dotSize.width}
+                height={dotSize.height}
+            />
+
+            {/* Bottom Dot */}
+            <Rect
+                x={Selector(canvasCenter, v => v.x - dotSize.width / 2)}
+                y={Selector(canvasSize, v => v.height - dotSize.height / 2)}
+                width={dotSize.width}
+                height={dotSize.height}
+            />
+        </Group>
+    )
+}
--- src/components/MyCanvas.tsx
+++ src/components/MyCanvas.tsx
@@ -3,63 +3,35 @@ import { LayoutChangeEvent } from "react-native";
 import { Canvas, Circle, Group, Rect, Selector, Text, interpolate, mix, point, size, useClockValue, useComputedValue, useFont, useSharedValueEffect, useValue, useValueEffect } from "@shopify/react-native-skia";
 import { Vector2, Vector2Zero } from "src/core";
 import { RoundedBox } from "./RoundedBox";
+import { DebugGroup } from "./DebugGroup";
 
 // Note 1: Top Left is (0, 0)
 // Note 2: Using Reanimated 2 due to Expo not supporting Reanimated 3 yet.
 const MyCanvas = () => {
-    const fontSize = 24;
-    const font = useFont(require("./Roboto/Roboto-Regular.ttf"), fontSize);
-    // console.log(font);
-
     const clock = useClockValue();
-    const previousClock = useValue(0);
-    const FPS = useValue(0);
-    const FPS_Display = useComputedValue(() => `FPS: ${FPS.current}`, [FPS])
-
-    // Called every frame
-    useValueEffect(clock, () => {
-        const deltaTime = clock.current - previousClock.current;
-
-        if (yPosition.current < canvasCenter.current.y)
-            yPosition.current += 0.5 * deltaTime
-
-        FPS.current = Math.round(1000 / (deltaTime));
-        previousClock.current = clock.current;
-    });
-
     const yPosition = useValue(0);
     const canvasSize = useValue({ width: 0, height: 0 });
+
     const canvasCenter = useComputedValue(() => {
         return point(canvasSize.current.width / 2, canvasSize.current.height / 2)
     }, [canvasSize]);
 
-    const rectSize = size(64, 128);
+    useValueEffect(clock, () => {
+        if (yPosition.current < canvasCenter.current.y)
+            yPosition.current = interpolate(clock.current, [0, 10], [0, 5]);
+    });
 
-    if (font == null) return;
+    const rectSize = size(64, 128);
 
     return (
         <Canvas style={{ flex: 1, backgroundColor: "#222" }} onSize={canvasSize}>
-            <RoundedBox centerPosition={{ x: Selector(canvasCenter, (v) => v.x), y: yPosition }} size={{ x: rectSize.width, y: rectSize.height }} radius={10} />
+            <RoundedBox
+                centerPosition={{ x: Selector(canvasCenter, (v) => v.x), y: yPosition }}
+                size={{ x: rectSize.width, y: rectSize.height }}
+                radius={10}
+            />
 
-            {/* --- Debug Stuff --- */}
-            <Group color={"red"}>
-                <Text x={10} y={50} text={FPS_Display} font={font} />
-                {/* Middle Dot */}
-                <Rect
-                    x={Selector(canvasCenter, (v) => v.x - 5)}
-                    y={Selector(canvasCenter, (v) => v.y - 5)}
-                    width={10}
-                    height={10}
-                />
-                {/* Top Dot */}
-                <Rect
-                    x={Selector(canvasCenter, (v) => v.x - 5)}
-                    y={-5}
-                    width={10}
-                    height={10}
-                />
-            </Group>
-            {/* ------------------ */}
+            <DebugGroup canvasSize={canvasSize} />
         </Canvas>
     )
 }
