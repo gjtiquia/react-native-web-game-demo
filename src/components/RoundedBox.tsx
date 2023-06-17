e8803d5df51b0e519eb90e72a597f11614928ae4
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-18 01:15:33 +0800

    feat: improved interpolation
    using the position at the previous tick, position at the current tick and the elapsed time since previous tick, the rendered position is lerps between the positions
    
    references:
    - https://gamedev.stackexchange.com/questions/12754/how-to-interpolate-between-two-game-states
    - https://thinkingbox.medium.com/6-simple-js-math-functions-you-can-use-everyday-68f8d5b58514

--- src/components/RoundedBox.tsx
+++ src/components/RoundedBox.tsx
@@ -1,6 +1,6 @@
 import { RoundedRect, useValue, Group, SkiaValue, SkSize, Selector } from "@shopify/react-native-skia";
 import { skiaConfig } from "src/config/skiaConfig";
-import { WorldToCanvas, useRender } from "src/core";
+import { Time, WorldToCanvas, useRender } from "src/core";
 
 export const RoundedBox = ({ canvasSize }: { canvasSize: SkiaValue<SkSize> }) => {
     const width = 64; // Hardcode
@@ -10,15 +10,21 @@ export const RoundedBox = ({ canvasSize }: { canvasSize: SkiaValue<SkSize> }) =>
     const isInitialized = useValue(false);
 
     const centerY = useValue(0); // initial y is out of canvas, so will not flicker when snapping in place
-    const DEBUG_centerY = useValue(0);
+    const DEBUG_CURRENT_centerY = useValue(0);
+    const DEBUG_PREVIOUS_centerY = useValue(0);
 
-    useRender((gameEngine) => {
+    useRender((gameEngine, deltaTime, elapsedTime) => {
         const box = gameEngine.scene.findGameObject("boxInstance");
         if (!box) return;
 
         // Dynamically get y from game engine
         const { y: targetCanvasY } = WorldToCanvas(
             box.transform.position,
+            { x: canvasSize.current.width, y: canvasSize.current.height }
+        );
+
+        const { y: previousCanvasY } = WorldToCanvas(
+            box.transform.previousPosition,
             { x: canvasSize.current.width, y: canvasSize.current.height }
         );
 
@@ -31,16 +37,27 @@ export const RoundedBox = ({ canvasSize }: { canvasSize: SkiaValue<SkSize> }) =>
 
         // Interpolate
         else {
-            // TODO : Snap when in a short distance
-            const distance = targetCanvasY - centerY.current;
+            const distance = targetCanvasY - previousCanvasY;
 
             if (Math.abs(distance) > skiaConfig.interpolationThreshold)
                 centerY.current = targetCanvasY;
-            else
-                centerY.current += distance * skiaConfig.interpolationStrength;
+            else {
+                // centerY.current += distance * skiaConfig.interpolationStrength;
+
+                const alpha = elapsedTime / Time.fixedDeltaTime;
+                const a = previousCanvasY;
+                const b = targetCanvasY;
+
+                const lerped = a + alpha * (b - a);
+
+                centerY.current = lerped;
+            }
         }
 
-        if (skiaConfig.debugMode) DEBUG_centerY.current = targetCanvasY;
+        if (skiaConfig.debugMode) {
+            DEBUG_CURRENT_centerY.current = targetCanvasY;
+            DEBUG_PREVIOUS_centerY.current = previousCanvasY;
+        }
     })
 
     return (
@@ -60,15 +77,26 @@ export const RoundedBox = ({ canvasSize }: { canvasSize: SkiaValue<SkSize> }) =>
             />
 
             {skiaConfig.debugMode ?
-                <RoundedRect
-                    x={Selector(canvasSize, v => v.width / 2)}
-                    y={DEBUG_centerY}
-                    width={width}
-                    height={height}
-                    r={radius}
-                    color={"red"}
-                    opacity={0.5}
-                /> : null
+                <>
+                    <RoundedRect
+                        x={Selector(canvasSize, v => v.width / 2)}
+                        y={DEBUG_CURRENT_centerY}
+                        width={width}
+                        height={height}
+                        r={radius}
+                        color={"red"}
+                        opacity={0.5}
+                    />
+                    <RoundedRect
+                        x={Selector(canvasSize, v => v.width / 2)}
+                        y={DEBUG_PREVIOUS_centerY}
+                        width={width}
+                        height={height}
+                        r={radius}
+                        color={"green"}
+                        opacity={0.5}
+                    />
+                </> : null
             }
         </Group>
     )
--- src/config/gameEngineConfig.ts
+++ src/config/gameEngineConfig.ts
@@ -2,7 +2,7 @@ import { GameEngine, GameEngineConfig } from "src/core";
 import { sceneConfig } from "./sceneConfig";
 
 export const gameEngineConfig: GameEngineConfig = {
-    tickRate: 2, // Slower TPS to for debugging
+    tickRate: 3, // Slower TPS to for debugging
     // tickRate: 10, // Slow TPS for less computational load
     // tickRate: 20, // Minecraft is at 20TPS, 
     // tickRate: 60, // Faster TPS to match 60Hz refresh rate and have less input lag
