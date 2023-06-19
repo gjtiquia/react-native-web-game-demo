b5c01300e6bbf8bfb16ede6a85b6b24718765500
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-19 12:04:48 +0800

    chore: attempt to fix circular dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from "./config" // Define this first to prevent circular dependencies
+// export * from "./core" // TODO : Solve circular dependencies
+export * from "./config" // Define this first to prevent circular dependencies when using InputAction in main
 export * from "./Main"
